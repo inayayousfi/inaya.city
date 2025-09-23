bcfd09b009c2c35522acff423bfbb7a4f2705dcc
ZiedYousfi <yousfi_zied@yahoo.com>
2025-09-23 17:37:55 +0200

    Update footer and header components with new social links and copyright information; change site title and description in consts.ts

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = "Zied's Cavern";
+export const SITE_DESCRIPTION =
+  "Welcome to Zied's Cavern! A personal website where I share my thoughts, experiences, and projects. Dive into my world of tech and art.";
